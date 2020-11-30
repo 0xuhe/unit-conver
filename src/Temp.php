1dd25b25c0a6ffdef0f91517abcf37ea6bec743f
hexu <251323229@qq.com>
2020-11-30 19:28:28 +0800

    add temp

---
+++ src/Temp.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Hexu\UnitConvert;
+
+
+class Temp
+{
+    private float $c;
+
+    public function __construct(float $c)
+    {
+        $this->c = $c;
+    }
+
+    public static function fromC(float $c)
+    {
+        return new static($c);
+    }
+
+    public function toF()
+    {
+        return $this->c * 1.8 + 32;
+    }
+}
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Hexu\UnitConvert\Tests;
 
+use Hexu\UnitConvert\Temp;
 use Hexu\UnitConvert\Weight;
 use PHPUnit\Framework\TestCase;
 
@@ -11,5 +12,11 @@ class ExampleTest extends TestCase
     public function convert_weight_from_kg_to_lbs()
     {
         $this->assertEquals(4.4, Weight::fromKg(2)->toLbs());
+    }
+
+    /** @test */
+    public function convert_temp_from_c_to_f()
+    {
+        $this->assertEquals(35.6, Temp::fromC(2)->toF());
     }
 }
